41df47140dcf0d1c5bcb411bd53798f9204a09c2
xoHalara <jaiimilaxo@gmail.com>
2025-05-04 23:27:04 -0500

    fixed bugs

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ from datetime import timedelta
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = 'your-secret-key-here'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret-jwt-key'
+    JWT_SECRET_KEY = 'super-secret-jwt-key'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
